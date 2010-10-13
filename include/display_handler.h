df10306125c4ae23711b8ee77d80bdf157362ec0
Jani Koskinen <janikoskinen@iki.fi>
2010-10-13 23:19:18 +0300

    Headers for display and keypad APIs

--- include/display_handler.h
+++ include/display_handler.h
@@ -7,20 +7,21 @@ typedef struct display_handler display_handler_t;
 typedef struct display_scroller display_scroller_t;
 
 struct display_scroller {
-  io_watcher watcher;
+  ev_io watcher;
   display_scroller_t *next;
-}
+};
 
 struct display_handler {
-  char *disp_name;
+  struct ev_loop *loop;
   int disp_fd;
-
   display_scroller_t *scrollers;
 };
 
 
 /* *** API Definition *** */
 
+display_handler_t *display_handler_init(struct ev_loop*, int);
+void display_handler_close(display_handler_t*);
 
 int display_handler_clear_display();
 int display_handler_write(char *);
--- include/keypad_handler.h
+++ include/keypad_handler.h
@@ -2,17 +2,28 @@
 #define _KEYPAD_HANDLER_H
 
 
-typedef struct keypad_handler keypad_handler_t:
+typedef struct keypad_handler keypad_handler_t;
 
-typedef void (*key_received_callback_t) (void*);
+typedef void (*key_received_callback_t) (keypad_handler_t *,
+					 char);
+
 
 struct keypad_handler {
-  char *dev_name;
+  ev_io dev_watcher;
+  struct ev_loop *loop;
   int dev_fd;
   key_received_callback_t key_callback;
 };
 
-int keypad_handler_set_received_key_cb();
+
+/* API */
+
+keypad_handler_t *keypad_handler_init(struct ev_loop *, int);
+void keypad_handler_close(keypad_handler_t*);
+
+
+int keypad_handler_set_key_received_callback(keypad_handler_t *,
+					     key_received_callback_t);
 
 int keypad_handler_get_column(char key);
 int keypad_handler_get_row(char key);
