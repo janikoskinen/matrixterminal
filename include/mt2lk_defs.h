14c3b57d23800985967761948cc15adf0f918e6e
Jani Koskinen <janikoskinen@iki.fi>
2010-10-18 16:59:04 +0300

    Added few lk commands

--- include/mt2lk_defs.h
+++ include/mt2lk_defs.h
@@ -6,6 +6,10 @@
 #define KEY_LEFT 
 #define KEY_RIGHT 
 
-#define LKCMD_SET_POS "\xfe\47%d%d"
+#define LKCMD_SET_POS "\xfe\x47%d%d"
+#define LKCMD_CLR_SRC "\xfe\x58"
+
+#define LKCMD_GPO_OFF "\xfe\x56%d"
+#define LKCMD_GPO_ON  "\xfe\x57%d"
 
 #endif
