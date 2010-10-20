04e0133814e3380b14168f0cfb184a3eeb83de76
Jani Koskinen <janikoskinen@iki.fi>
2010-10-21 00:27:07 +0300

    Some definitions added for keypad handling

--- include/mt2lk_defs.h
+++ include/mt2lk_defs.h
@@ -1,22 +1,47 @@
 #ifndef _MT2LK_DEFS_H
 #define _MT2LK_DEFS_H
 
-#define KEY_UP 
-#define KEY_DOWN 
-#define KEY_LEFT 
-#define KEY_RIGHT 
+/* Key definitions */
 
-#define LKCMD_SET_POS "\xfe\x47%d%d" // Row (1-4), Col (1-20)
+#define LKKEY_UP 
+#define LKKEY_DOWN 
+#define LKKEY_LEFT 
+#define LKKEY_RIGHT 
+
+#define LKKEY_0
+#define LKKEY_1
+#define LKKEY_2
+#define LKKEY_3
+#define LKKEY_4
+#define LKKEY_5
+#define LKKEY_6
+#define LKKEY_7
+#define LKKEY_8
+#define LKKEY_9
+
+#define LKKEY_ENTER
+#define LKKEY_BACK
+
+#define LKKEY_A
+#define LKKEY_B
+#define LKKEY_C
+#define LKKEY_D
+
+
+
+/* Module command definitions */
+
+#define LKCMD_SET_POS "\xfe\x47%c%c" // Row (1-4), Col (1-20)
 #define LKCMD_CLR_SRC "\xfe\x58"
 
-#define LKCMD_GPO_ON  "\xfe\x57%d" // nGPO (1-6)
-#define LKCMD_GPO_OFF "\xfe\x56%d" // nGPO (1-6)
+#define LKCMD_GPO_ON  "\xfe\x57%c" // nGPO (1-6)
+#define LKCMD_GPO_OFF "\xfe\x56%c" // nGPO (1-6)
 
 #define LKCMD_BACKLIGHT_ON  "\xfe\x42"
 #define LKCMD_BACKLIGHT_OFF "\xfe\x46"
-#define LKCMD_SET_CONTRAST  "\xfe\x50%d" // Contrast (0-255)
+#define LKCMD_SET_CONTRAST  "\xfe\x50%c" // Contrast (0-255)
 
-#define LKCMD_AUTO_REPEAT_ON  "\xfe\x7e%d" // 0: 200ms repeat, 1: Up/down sent
+#define LKCMD_AUTO_REPEAT_ON  "\xfe\x7e%c" // 0: 200ms repeat, 1: Up/down sent
 #define LKCMD_AUTO_REPEAT_OFF "\xfe\x60"
 #define LKCMD_AUTO_SCROLL_ON  "\xfe\x51"
 #define LKCMD_AUTO_SCROLL_OFF "\xfe\x52"
@@ -31,8 +56,8 @@
 #define LKCMD_VBAR_INIT  "\xfe\x76"
 #define LKCMD_VBAR2_INIT "\xfe\x73"
 #define LKCMD_HBAR_INIT  "\xfe\x68"
-#define LKCMD_VBAR_DRAW  "\xfe\x3d%d%d" // Col (1-20), Length (0-20)
-#define LKCMD_HBAR_DRAW  "\xfe\x7c%d%d%d%d" // Col (1-20),
+#define LKCMD_VBAR_DRAW  "\xfe\x3d%c%c" // Col (1-20), Length (0-20)
+#define LKCMD_HBAR_DRAW  "\xfe\x7c%c%c%c%c" // Col (1-20),
                                             // Row (1-4),
                                             // Dir (0: right, 1: left)
                                             // Length (0-100)
