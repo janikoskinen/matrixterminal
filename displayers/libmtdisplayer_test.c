3b88185dc781d8b76ac1867c20c7b82b07a17257
Jani Koskinen <janikoskinen@iki.fi>
2010-11-16 14:06:21 +0200

    Implementation for test_displayer

--- displayers/libmtdisplayer_test.c
+++ displayers/libmtdisplayer_test.c
@@ -1,22 +1,53 @@
 #include "displayer_api.h"
 
+#define DEBUG
+
+#include "debug.h"
+
+#define TIMER_TIME 1.0
+#define TIMER_REPEAT 1.0
+
+
+int displayer_initialize(displayer_t *disp);
+int displayer_start(displayer_t *disp);
+int displayer_stop(displayer_t *disp);
+
+
+static void test_runner(struct ev_loop *l, ev_timer *w, int revents)
+{
+  DBG("Test timer...");
+}
+
 
 int displayer_initialize(displayer_t *disp)
 {
-
+  DBG("Initing test displayer");
+  ev_timer_init(&disp->disp_watcher, test_runner, TIMER_TIME, TIMER_REPEAT);
   return -1;
 }
 
 
 int displayer_start(displayer_t *disp)
 {
-
-  return -1;
+  DBG("Start test displayer");
+  if ((disp->status & DISPLAYER_STATUS_RUNNING) != 0) {
+    return -1;
+  } else {
+    DBG("Calling ev_timer_start");
+    ev_timer_start(disp->loop, &disp->disp_watcher);
+    DBG("Setting status");
+    disp->status |= DISPLAYER_STATUS_RUNNING;
+    return 0;
+  }
 }
 
 
 int displayer_stop(displayer_t *disp)
 {
-
-  return -1;
+  if ((disp->status & DISPLAYER_STATUS_RUNNING) == 0) {
+    return -1;
+  } else {
+    disp->status &= !DISPLAYER_STATUS_RUNNING;
+    return 0;
+  }
 }
